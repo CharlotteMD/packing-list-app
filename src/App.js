cf6ecb77889729b28ff2c1ede2744832f9d85294
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2020-03-26 14:30:15 +0000

    Reverted to having countries as a json list and not from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ThemeProvider } from 'styled-components'
+
+import countriesList from './countries.json'
+import { Header } from './components/App/Header.js'
+import { Footer } from './components/App/Footer.js'
+import BasicOutfits from './components/items/BasicOutfits'
+// import Essentials from './components/items/Essentials'
+
 import { BasicButton, EmojiSpan } from './style/global-styles'
 import { CountText } from './style/App-styles'
 import SummerTheme from './style/themes/summerTheme'
 import WinterTheme from './style/themes/winterTheme'
-import { Rotate } from './style/keyframe-animations'
-import { Header } from './components/App/Header.js'
-import { Footer } from './components/App/Footer.js'
-import BasicOutfits from './components/items/BasicOutfits'
-import Essentials from './components/items/Essentials'
 
 const App = () => {
   const [count, setCount] = useState(0)
-  const [leaveUk, setLeaveUk] = useState()
-  const [weather, setWeather] = useState('not sure')
+  // const [weather, setWeather] = useState('not sure')
   const [theme, setTheme] = useState('SummerTheme')
   const [location, setLocation] = useState('london')
   const [countries, setCountries] = useState()
+  const [holidayDestination, setHolidayDestination] = useState()
 
   const summerHoliday = () => {
     setTheme('SummerTheme')
@@ -27,49 +29,49 @@ const App = () => {
     setTheme('WinterTheme')
   }
 
-  async function getWeather() {
-    try {
-      await axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
-          {
-            crossdomain: true
-          }
-        )
-        .then((result) => {
-          const currentWeather = result.data.weather[0].description
-          setWeather(currentWeather)
-        })
-    } catch (e) {
-      console.error(e)
-    }
-  }
+  // async function getWeather() {
+  //   try {
+  //     await axios
+  //       .get(
+  //         `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
+  //         {
+  //           crossdomain: true
+  //         }
+  //       )
+  //       .then((result) => {
+  //         const currentWeather = result.data.weather[0].description
+  //         setWeather(currentWeather)
+  //       })
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  // }
 
-  async function getCountry() {
-    try {
-      await axios
-        .get('https://restcountries.eu/rest/v2/all?fields=name;', {
-          crossdomain: true
-        })
-        .then((result) => {
-          let countriesList = []
-          const countryInformation = result.data
-
-          countryInformation.forEach((countries) =>
-            countriesList.push(countries.name)
-          )
-          setCountries(countriesList)
-        })
-    } catch (e) {
-      console.error(e)
-    }
-  }
+  // async function getCountry() {
+  //   try {
+  //     await axios
+  //       .get('https://restcountries.eu/rest/v2/all?fields=name;', {
+  //         crossdomain: true
+  //       })
+  //       .then((result) => {
+  //         const countryInformation = result.data
+  //         let countriesList = []
+  //         countryInformation.forEach((countries) =>
+  //           countriesList.push(countries.name)
+  //         )
+  //         setCountries(countriesList)
+  //       })
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  // }
 
   useEffect(() => {
-    getWeather()
+    // getWeather()
+    // getCountry()
   })
 
-  getCountry()
+  console.log(countriesList)
 
   return (
     <ThemeProvider theme={theme === 'SummerTheme' ? SummerTheme : WinterTheme}>
@@ -81,30 +83,19 @@ const App = () => {
 
         <div className="questions">
           <p>Where are you going?</p>
-          {!!countries && (
-            <ul>
-              {countries.map((i) => (
-                <li key={i}>{i}</li>
-              ))}
-            </ul>
-          )}
+          {!!holidayDestination && <p>I'm going to {holidayDestination}</p>}
+          <ul>
+            {countriesList.map((country) => (
+              <button key="country" onClick={() => setHolidayDestination(country)}>
+                {country}
+              </button>
+            ))}
+          </ul>
 
-          {!!leaveUk && (
-            <Rotate>
-              <EmojiSpan ariaRef="leave-UK">🛫</EmojiSpan>
-            </Rotate>
-          )}
-          {!leaveUk && <EmojiSpan ariaRef="UK">🇬🇧</EmojiSpan>}
-          <BasicButton textButton onClick={() => setLeaveUk(false)}>
-            Staying in the UK
-          </BasicButton>
-          <BasicButton textButton onClick={() => setLeaveUk(true)}>
-            Going far away
-          </BasicButton>
           <div className="location">
             <h2>Where are you going?</h2>
             <p>I'm going to {location}</p>
-            {weather && <p>The weather is {weather}</p>}
+            {/* {weather && <p>The weather is {weather}</p>} */}
 
             <BasicButton onClick={() => setLocation('london')}>
               <EmojiSpan ariaRef="UK">🇬🇧</EmojiSpan>
@@ -127,7 +118,7 @@ const App = () => {
         </div>
         <div className="packingList">
           <h1>You should take</h1>
-          <Essentials leaveUk={leaveUk} />
+          {/* <Essentials leaveUk={leaveUk} /> */}
           <BasicOutfits count={count} />
         </div>
         <Footer />
