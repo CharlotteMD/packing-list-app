4401d4df6ae85382dc135f640a4e5c4fa6bd9d05
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2020-04-01 09:57:12 +0100

    Added lat and lng states

--- src/Questions.js
+++ src/Questions.js
@@ -7,6 +7,8 @@ export const Questions = () => {
   const [holidayDestination, setHolidayDestination] = useState()
   const [updatingCountry, setUpdatingCountry] = useState(false)
   const [fullCountryData, setFullCountryData] = useState()
+  const [lat, setLat] = useState()
+  const [lng, setLng] = useState()
 
   async function getCountryInformation(holidayDestination) {
     try {
@@ -16,9 +18,10 @@ export const Questions = () => {
         })
         .then((result) => {
           const countryData = result.data
-          setFullCountryData(countryData)
+          const latlng = countryData[0].latlng
+          setFullCountryData(countryData[0])
           setUpdatingCountry(false)
-          return countryData
+          return latlng
         })
     } catch (e) {
       console.error(e)
@@ -26,14 +29,22 @@ export const Questions = () => {
   }
 
   const handleCountryChange = (country) => {
-    setHolidayDestination(country)
-    setUpdatingCountry(true)
+    setHolidayDestination(country, setUpdatingCountry(true))
   }
 
   useEffect(() => {
     if (!!updatingCountry) {
       getCountryInformation(holidayDestination)
-      console.log(fullCountryData)
+    } else {
+    }
+  })
+
+  useEffect(() => {
+    if (!!fullCountryData) {
+      const newCountryLat = fullCountryData.latlng[0]
+      const newCountryLng = fullCountryData.latlng[1]
+      setLat(newCountryLat)
+      setLng(newCountryLng)
     } else {
     }
   })
